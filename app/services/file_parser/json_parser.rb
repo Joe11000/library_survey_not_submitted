55c0db0aee2688e0ae709a63f6b5e751dfc06937
Joe Noonan <joenoonan27@gmail.com>
2019-03-03 17:24:53 -0600

    FileParser::JSONParser created

--- app/services/file_parser/json_parser.rb
+++ app/services/file_parser/json_parser.rb
@@ -4,7 +4,14 @@ module FileParser
   class JSONParser
     def self.call file 
       json_text = File.read(file)
-      JSON.parse(json_text)
+      hash = JSON.parse(json_text)
+
+      # return consistent result syntax as multi upload and single upload
+      if(hash.dig('records', 'record').is_a? Array )
+        return hash['records']['record']
+      else
+        return [ hash['records']['record'] ]
+      end
     end
   end
 end
