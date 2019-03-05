a2a5ed7e7d445ae5f05bf629be0e0cf1f3f2bebb
Joe Noonan <joenoonan27@gmail.com>
2019-03-05 14:11:38 -0600

    request specs for report file upload

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -2,50 +2,102 @@ require 'rails_helper'
 
 RSpec.describe "Reports", type: :request do
   describe "html" do 
-    describe 'form submission' do 
-      describe "POST /reports" do
-        describe 'single record upload' do 
-          let(:file_location) { './spec/shared/json_files/single_record_upload.json' }
-          let(:reports) { FileParser::JSONParser.call( file_location ) }
-
-          it "" do
-            headers = {"Accept" => 'application/json'}
-            post reports_path, params: { "records" => reports }, headers: headers
-
-            expect(response).to have_http_status(200)
-            expect(response.body).to include "Total Pages Read"
-          end
-        end
-        describe 'multiple record upload' do 
-          let(:file_location) { './spec/shared/json_files/multi_record_upload.json' }
-          let(:reports) { FileParser::JSONParser.call( file_location ) }
-
-          it "" do
-            headers = {"Accept" => 'application/json'}
-            post reports_path, params: { "records" => reports }, headers: headers
-
-            expect(response).to have_http_status(200)
-            expect(response.body).to eq 
-          end
-        end
-      end
+    def get_single_record 
+      FileParser::JSONParser.call( Rails.root.join( 'spec', 'shared', 'json_files', 'single_record_upload.json' ) )
     end
 
     describe 'file submission' do 
       describe 'json file upload' do 
-        it '' do 
-          get root_path
-          
+        describe 'single record upload' do 
+          it 'is valid' do 
+            json_file_location = Rails.root.join( 'spec', 'shared', 'json_files', 'single_record_upload.json' )
+            mime_type = 'application/json'
+            upload_file = Rack::Test::UploadedFile.new(json_file_location, mime_type)
 
+            params =  {
+                        'records' => { 'file' => upload_file }
+                      }
+
+            expected_result = {
+                                "category_breakdown" => {
+                                    "000" => {
+                                        "name" => "Computer Science, Information & General Works",
+                                        "pages_read" => 1
+                                    }
+                                },
+                                "total_pages_read" => 1
+                              }
+            headers = {
+                        "ACCEPT" => "application/json",     # This is what Rails 4 accepts
+                        "CONTENT_TYPE" => "application/json"
+                      }
+            
+            post reports_path , params: params, headers: headers
+            expect(JSON.parse(response.body)).to eq expected_result
+          end
         end
       end
       
       describe 'xml file upload' do 
-      
+        describe 'single record upload' do 
+          it 'is valid' do 
+            xml_file_location = Rails.root.join( 'spec', 'shared', 'xml_files', 'single_record_upload.xml' )
+            mime_type = 'text/xml'
+            upload_file = Rack::Test::UploadedFile.new(xml_file_location, mime_type)
+
+            params =  {
+                        'records' => { 'file' => upload_file }
+                      }
+
+            expected_result = {
+                                "category_breakdown" => {
+                                    "000" => {
+                                        "name" => "Computer Science, Information & General Works",
+                                        "pages_read" => 1
+                                    }
+                                },
+                                "total_pages_read" => 1
+                              }
+            headers = {
+                        "ACCEPT" => "application/json",     # This is what Rails 4 accepts
+                        "CONTENT_TYPE" => "text/xml"
+                      }
+            
+            post reports_path , params: params, headers: headers
+            expect(JSON.parse(response.body)).to eq expected_result
+          end
+        end
       end
       
       describe 'csv file upload' do 
-      
+        describe 'single record upload' do 
+          it 'is valid' do 
+            csv_file_location = Rails.root.join( 'spec', 'shared', 'csv_files', 'single_record_upload.csv' )
+            mime_type = 'application/csv'
+            upload_file = Rack::Test::UploadedFile.new(csv_file_location, mime_type)
+
+            params =  {
+                        'records' => { 'file' => upload_file }
+                      }
+
+            expected_result = {
+                                "category_breakdown" => {
+                                    "000" => {
+                                        "name" => "Computer Science, Information & General Works",
+                                        "pages_read" => 1
+                                    }
+                                },
+                                "total_pages_read" => 1
+                              }
+            headers = {
+                        "ACCEPT" => "application/json",     # This is what Rails 4 accepts
+                        "CONTENT_TYPE" => "application/csv"
+                      }
+            
+            post reports_path , params: params, headers: headers
+            expect(JSON.parse(response.body)).to eq expected_result
+          end
+        end
       end
     end
   end
